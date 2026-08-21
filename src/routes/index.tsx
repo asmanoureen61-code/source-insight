d02f3fac5deba0afbce884a7dc77571b087716fe
Asma Noureen <asmanoureen61@gmail.com>
2026-08-21 16:12:30 +0500

    style: load shared RAG form styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { RagApp } from "@/components/rag-app";
+import "../form.css";
 
 export const Route = createFileRoute("/")({
   component: Index,
